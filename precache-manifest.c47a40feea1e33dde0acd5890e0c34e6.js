7c3827466af499a0f0735189d281ecad3d04adf8
ANJARASOA Bernardin <bernardinanjarasoa@hotmail.com>
2022-08-11 20:20:55 +0300

    Updates

---
+++ precache-manifest.c47a40feea1e33dde0acd5890e0c34e6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5a41acaf92780eaa98090b95d84a4f",
+    "url": "/quiz-mada-marketing/index.html"
+  },
+  {
+    "revision": "e6f8e6e1bab03120f3d7",
+    "url": "/quiz-mada-marketing/static/css/2.822cae94.chunk.css"
+  },
+  {
+    "revision": "249d77760faf8e28cb34",
+    "url": "/quiz-mada-marketing/static/css/main.05ea2b23.chunk.css"
+  },
+  {
+    "revision": "e6f8e6e1bab03120f3d7",
+    "url": "/quiz-mada-marketing/static/js/2.5176fc1c.chunk.js"
+  },
+  {
+    "revision": "249d77760faf8e28cb34",
+    "url": "/quiz-mada-marketing/static/js/main.a80ca159.chunk.js"
+  },
+  {
+    "revision": "04af6acd769b8b4a6f3b",
+    "url": "/quiz-mada-marketing/static/js/runtime-main.a6aefece.js"
+  },
+  {
+    "revision": "f05e37d5dde17938e5efbd6ff5876374",
+    "url": "/quiz-mada-marketing/static/media/answer.f05e37d5.png"
+  },
+  {
+    "revision": "6e55ab4f62ed609ab3066958c6f9a96b",
+    "url": "/quiz-mada-marketing/static/media/background.6e55ab4f.jpg"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/quiz-mada-marketing/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/quiz-mada-marketing/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "f06f3bfe9454949bce2baa2f2af4d2ec",
+    "url": "/quiz-mada-marketing/static/media/fiftyFifty.f06f3bfe.PNG"
+  },
+  {
+    "revision": "145bf2178ec018524dadd29e831cc8a3",
+    "url": "/quiz-mada-marketing/static/media/hints.145bf217.PNG"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/quiz-mada-marketing/static/media/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/quiz-mada-marketing/static/media/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/quiz-mada-marketing/static/media/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/quiz-mada-marketing/static/media/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "0ab14d143f1993fd0a065be0e0aa58a5",
+    "url": "/quiz-mada-marketing/static/media/options.0ab14d14.PNG"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/quiz-mada-marketing/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
